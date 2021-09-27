23c83def3d04d8d011a6df0d9dcedf2fb1727a5b
Keith Naber <knaber4124@gmail.com>
2021-09-27 12:25:54 -0400

    beginning auth and database tie in

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+
 
 // route GET api/auth
 // Get Logged in User
@@ -12,8 +18,47 @@ router.get('/', (req, res) => {
 // Auth User and Get Token
 // Public Access
 
-router.post('/', (req,res)=>{
-    res.send('Log In User');
+router.post('/', [
+    check('email', 'please include a valid email').isEmail(),
+    check('password', 'password is required').exists()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                msg: 'Invalid Credentials'
+            });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                msg: 'Invalid Credentials'
+            })
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 3600000,
+        },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 
 const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to Contact Keeper API'}));
+app.get('/', (req, res) => res.json({ msg: 'Welcome to Contact Keeper API' }));
 
 // Routes
-app.use('/api/users', require ('./routes/users'));
-app.use('/api/contacts', require ('./routes/contacts'));
-app.use('/api/auth', require ('./routes/auth'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
